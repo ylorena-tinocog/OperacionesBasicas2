0d7136f03abdaef292119a9b2e371fe555b02a93
lorena <lorena@lorena-PC>
2016-09-28 00:18:58 -0500

    Proyecto operaciones primera vez

---
+++ src/main/java/org/ucc/operaciones/Operaciones.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ucc.operaciones;
+
+/**
+ *
+ * @author lorena
+ */
+public class Operaciones {
+    
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,6 +8,6 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Ejemplo github</h1>
     </body>
 </html>
